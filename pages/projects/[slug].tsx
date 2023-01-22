1e0300825adf593f226ec73cd6a1e20cac61756e
Brian Behrens <brian@brainbrian.com>
2023-01-21 19:11:54 -0800

    add projects to base of app

---
+++ pages/projects/[slug].tsx
@@ -0,0 +1,126 @@
+import { format } from 'date-fns';
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import React from 'react';
+import rehypeParse from 'rehype-parse';
+import rehypeReact from 'rehype-react';
+import { unified } from 'unified';
+import rehypeStringify from 'rehype-stringify';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+
+import { Footer, Head, Header } from '../../components'; // Pagination
+
+export interface PostProps {
+    categories?: string[];
+    content?: string;
+    date: string;
+    dateFormatted: string;
+    slug: string;
+    tags?: string[];
+    title: string;
+}
+
+const Post = ({
+    content,
+    dateFormatted,
+    slug,
+    title,
+}: PostProps): React.ReactNode => {
+    const ResponsiveImage = (props: any) => {
+        return (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+                alt={props.alt}
+                {...props}
+                src={props.src.replaceAll('./', `/images/projects/${slug}/`)}
+            />
+        );
+    };
+
+    const processor = unified()
+        .use(rehypeParse)
+        .use(rehypeReact, {
+            createElement: React.createElement,
+            components: {
+                img: ResponsiveImage,
+            },
+        });
+
+    console.log(
+        'processor.processSync(content)',
+        processor.processSync(content),
+    );
+
+    return (
+        <>
+            <Head title={`${title} | Posts | Brian Behrens`} />
+            <Header />
+            <main className="content">
+                <div className="post-container">
+                    <article className="post">
+                        <h1>{title}</h1>
+                        <h3>{dateFormatted}</h3>
+                        <div className="post-content">
+                            {processor.processSync(content).result}
+                        </div>
+                        {/* <div
+                            className="post-content"
+                            dangerouslySetInnerHTML={{
+                                __html:
+                                    processor.processSync(content).result || '',
+                            }}
+                        /> */}
+                    </article>
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync(path.join('content/projects'));
+
+    const paths = files.map((file) => ({ params: { slug: file } }));
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({
+    params: { slug },
+}: {
+    params: { slug: string };
+}) {
+    const { data: frontmatter, content } = matter(
+        fs.readFileSync(
+            path.join(`content/projects/${slug}/`, 'index.md'),
+            'utf-8',
+        ),
+    );
+
+    const processor = unified()
+        .use(remarkParse)
+        .use(remarkRehype, {
+            sanitize: false,
+            allowDangerousHTML: true,
+        } as any)
+        .use(rehypeStringify);
+
+    return {
+        props: {
+            ...frontmatter,
+            slug,
+            dateFormatted: format(new Date(frontmatter.date), 'MMMM dd, yyyy'),
+            content: await processor
+                .process(content || '')
+                .then((file) => String(file)),
+        },
+    };
+}
+
+export default Post;
---
+++ pages/projects/index.tsx
@@ -0,0 +1,82 @@
+import { format } from 'date-fns';
+import fs from 'fs';
+import { orderBy } from 'lodash';
+import matter from 'gray-matter';
+import path from 'path';
+import React from 'react';
+// import remark from 'remark';
+// import html from 'remark-html';
+
+import { Footer, Head, Header, PostLink } from '../../components'; // Pagination
+
+interface PostProps {
+    categories?: string[];
+    date: string;
+    dateFormatted: string;
+    slug: string;
+    tags?: string[];
+    title: string;
+}
+
+const Posts = ({ posts }: { posts: PostProps[] }): React.ReactNode => {
+    const postsComponents = posts.map(
+        ({ dateFormatted, slug, title }, index) => (
+            <PostLink
+                key={index}
+                date={dateFormatted}
+                href={`/posts/${slug}`}
+                title={title}
+                excerpt="..."
+            />
+        ),
+    );
+    // const { currentPage, numPages } = pageContext;
+
+    return (
+        <>
+            <Head
+                title="Posts | Brian Behrens | Los Angeles Software Engineer"
+                description="Posts (blogs) from Brian Behrens of my adventures and journey in coding."
+            />
+            <Header />
+            <main className="content">
+                {postsComponents}
+                {/* <Pagination
+                    basePath="/posts/"
+                    currentPage={currentPage}
+                    numPages={numPages}
+                /> */}
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+export async function getStaticProps() {
+    const files = fs.readdirSync(path.join('content/projects'));
+
+    const posts = files.map((file) => {
+        const slug = file;
+
+        const { data: frontmatter } = matter(
+            fs.readFileSync(
+                path.join(`content/projects/${slug}/`, 'index.md'),
+                'utf-8',
+            ),
+        );
+
+        return {
+            ...frontmatter,
+            dateFormatted: format(new Date(frontmatter.date), 'MMMM dd, yyyy'),
+            slug,
+        };
+    });
+
+    return {
+        props: {
+            posts: orderBy(posts, 'date').reverse(),
+        },
+    };
+}
+
+export default Posts;
