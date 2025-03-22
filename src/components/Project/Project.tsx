b041e29b083753637fb67d13d85f3df6e19110db
Brian Behrens <brian@brainbrian.com>
2025-03-22 11:27:12 -0700

    refactor(Project): remove SCSS styles and convert Project component to use Tailwind CSS for layout and styling

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 
-import Styles from './Project.module.scss';
-
 interface ProjectProps {
     excerpt?: string;
     imageUrl: string;
@@ -16,16 +14,37 @@ export const Project: React.FC<ProjectProps> = ({
     slug,
     title,
 }) => (
-    <article className={Styles.Project}>
-        <Link href={slug} className={Styles.Link}>
-            <h2>{title}</h2>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            {imageUrl && <img src={imageUrl} alt={`${title} Sample`} />}
+    <article className="mb-8">
+        <Link
+            href={slug}
+            className="flex flex-col-reverse bg-darker relative transition-colors duration-200 hover:bg-main hover:no-underline focus-visible:bg-main focus-visible:no-underline group"
+        >
+            {}
+            {imageUrl && (
+                <img
+                    src={imageUrl}
+                    alt={`${title} Sample`}
+                    className="w-full"
+                />
+            )}
+            <div className="relative">
+                <h2 className="text-xl font-medium m-0 p-4 uppercase sm:text-2xl sm:translate-x-8 transition-all duration-1200 group-hover:text-light group-hover:sm:translate-x-12 group-focus-visible:text-light group-focus-visible:sm:translate-x-12">
+                    {title}
+                </h2>
+                <span className="hidden sm:block absolute bottom-8 left-4 h-0.5 w-4 bg-main transition-all duration-1200 group-hover:bg-light group-hover:w-8 group-focus-visible:bg-light group-focus-visible:w-8"></span>
+            </div>
         </Link>
         {excerpt && (
-            <p
-                className={Styles.Content}
-                dangerouslySetInnerHTML={{ __html: excerpt }}
+            <div
+                className="mt-4 mx-auto max-w-[56rem] px-4 text-lg"
+                dangerouslySetInnerHTML={{
+                    __html: excerpt
+                        .replace(
+                            /<a /g,
+                            '<a class="bg-main text-light py-1 px-1.5 no-underline leading-loose hover:bg-light hover:text-main focus-visible:bg-light focus-visible:text-main" ',
+                        )
+                        .replace(/<p>/g, '<p class="m-0 break-words">'),
+                }}
             />
         )}
     </article>
