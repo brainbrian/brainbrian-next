05e31334d99180830241ddef861632fad8cd238c
Brian Behrens <brian@brainbrian.com>
2025-03-23 00:14:24 -0700

    refactor(Header): update font weight in Header component for improved text styling and consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ export const Header: React.FC = () => {
                             <li className="relative" key={index}>
                                 <Link
                                     className={cx(
-                                        "flex items-center justify-center flex-1 text-text font-headline font-medium text-[0.7rem] h-[3rem] px-2 uppercase no-underline transition-colors duration-200 hover:bg-surface hover:no-underline focus-visible:bg-surface focus-visible:no-underline focus:outline-hidden relative after:content-[''] after:absolute after:bg-surface after:bottom-[-1rem] after:h-[1rem] after:left-0 after:w-full after:opacity-0 after:transition-opacity after:duration-200 after:z-2 sm:text-[0.75rem] sm:px-3 md:text-[1.125rem] md:h-[5rem] md:px-[1rem]",
+                                        "flex items-center justify-center flex-1 text-text font-headline font-semibold text-[0.7rem] h-[3rem] px-2 uppercase no-underline transition-colors duration-200 hover:bg-surface hover:no-underline focus-visible:bg-surface focus-visible:no-underline focus:outline-hidden relative after:content-[''] after:absolute after:bg-surface after:bottom-[-1rem] after:h-[1rem] after:left-0 after:w-full after:opacity-0 after:transition-opacity after:duration-200 after:z-2 sm:text-[0.75rem] sm:px-3 md:text-[1.125rem] md:h-[5rem] md:px-[1rem]",
                                         {
                                             'bg-surface after:opacity-100 after:animate-nav-animation':
                                                 pathname.includes(href),
